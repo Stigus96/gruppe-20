489cec7a2cd151ca875ad11d9270c98e92ba350c
Stigus96 <stighelgebruset@msn.com>
2020-09-09 12:58:19 +0200

    started on connect function

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,2 +1,19 @@
+import java.net.*;
+import java.io.*;
+
 public class TCPClient {
+    private Socket connection;
+
+    public boolean connect(Strig host, int port) {
+        try{
+            Socket socket = newSocket(host, port);
+            System.out.println("Successfully connected");
+
+            connection = socket;
+
+        } catch (IOException e) {
+            System.out.println("Socket error: " + e.getMessage());
+            lastError = "" + e;
+        }
+    }
 }
