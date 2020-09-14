0e39fce13839936f7902c85b975010408543f0a8
sigurdurhj80 <siggihallur2@gmail.com>
2020-09-14 21:46:11 +0200

    worked on GUI

---
+++ src/AppMain.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class AppMain extends Application {
+
+    private GUI gui = new GUI();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        setStage(primaryStage);
+        primaryStage.setScene(gui.getMainScene());
+        primaryStage.show();
+    }
+
+    /**
+     * launch the APP
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Method for Primary Stage control
+     * @param primaryStage
+     */
+    private void setStage(Stage primaryStage) {
+        primaryStage.setTitle("Camera Car Gruppe 20");
+        primaryStage.setX(50);
+        primaryStage.setY(50);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(600);
+    }
+}
